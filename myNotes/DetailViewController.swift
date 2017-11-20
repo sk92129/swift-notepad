58bf3381ccc5836170a2db99af483d3fe713361b
Sean Kang <seankang@gmail.com>
2017-11-19 17:27:30 -0800

    finish tutorial

---
+++ myNotes/DetailViewController.swift
@@ -0,0 +1,56 @@
+//
+//  DetailViewController.swift
+//  myNotes
+//
+//  Created by Sean Kang on 11/19/17.
+//  Copyright © 2017 ceg. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
+    
+    var text:String = ""
+    
+    var masterView:ViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        textView.text = text
+        
+        textView.becomeFirstResponder()
+    }
+    
+    func setText(t:String) {
+        text = t
+        if (isViewLoaded){
+            textView.text = t
+
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        masterView.newRowText = textView.text
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- myNotes/ViewController.swift
+++ myNotes/ViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var table: UITableView!
     var data: [String] = []
+    var selectedRow:Int = -1
+    var newRowText:String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -25,15 +28,34 @@ class ViewController: UIViewController, UITableViewDataSource {
         load()
     }
     
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if (selectedRow == -1 ){
+            return
+        }
+        data[selectedRow] = newRowText
+
+        if (newRowText == ""){
+            data.remove(at: selectedRow)
+        }
+        table.reloadData()
+        save()
+        
+    }
     @objc func addNote() {
         if (table.isEditing){
             return
         }
-        let name:String = "Row \(data.count + 1)"
+        let name:String = ""
         data.insert(name, at: 0)
         let indexPath:IndexPath = IndexPath(row:0, section: 0)
         table.insertRows(at: [indexPath], with: .automatic)
-        save()
+        
+        table.selectRow(at: indexPath, animated: true, scrollPosition: .none)
+        
+        // this will call the other view controller to show the other textview
+        self.performSegue(withIdentifier: "detail", sender: nil)
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -62,6 +84,21 @@ class ViewController: UIViewController, UITableViewDataSource {
         save()
     }
     
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // this will call the other view controller to show the other textview
+        self.performSegue(withIdentifier: "detail", sender: nil)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let detailView:DetailViewController = segue.destination as! DetailViewController
+        
+        selectedRow = table.indexPathForSelectedRow!.row
+        detailView.masterView = self
+        detailView.setText(t: data[selectedRow])
+        
+    }
     
     func save() {
         UserDefaults.standard.set(data, forKey: "notes")
